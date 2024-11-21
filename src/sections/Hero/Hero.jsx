fd80628d90e40d71b67495a80b17a4905b995936
xuanviet73 <dinhxuanviet12dll@gmail.com>
2024-11-21 21:08:32 +0700

    Update link social

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -42,13 +42,13 @@ function Hero() {
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
+          <a href="https://www.facebook.com/vietdnih73" target="_blank">
             <img src={facebookIcon} alt="Facebook icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/xuanviet73" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.instagram.com/vietdnih.73/?theme=dark" target="_blank">
             <img src={instagramIcon} alt="Linkedin icon" />
           </a>
         </span>
